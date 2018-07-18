015ae9d5cc7315ce73bbbd2eac63bc9531c16950
Matthew Williams <matthew@williams.hk>
2018-07-18 22:39:09 +0100

    matrix Background V2
    kinda fat but not nearly as bad as it could be

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/*
+
+Notes for new matrix:
+
+10-long snakes
+all same speed
+fade out
+decrement by fixed margin
+OCR-A font?
+Minecraft font?
+RS font?
+https://damieng.com/blog/2011/02/20/typography-in-8-bits-system-fonts
+https://www.deviantart.com/shabbygirlblog/art/Green-Matrix-Code-Gif-528227911
+
+*/
+
 
 $(function() {
     var ImageArray = ['img/code-bg-1.png','img/code-bg-2.png','img/code-bg-3.png','img/code-bg-4.png'];
@@ -7,11 +23,51 @@ $(function() {
 $(window).on('load', function(){
     var matrix = $('#matrixBackground');
 
-    function randomBinary() {
-        return ~~(Math.random() >= 0.5);
+    function randomAscii() {
+        return String.fromCharCode(Math.floor(Math.random() * 94 + 33));
     }
 
     //      Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+    function matrixRain(left) {
+        var a = $('<a class="mB">'+randomAscii()+'</a>');
+        $(a).css('left', left);
+        $(a).css('top', 0);
+        var rain = setInterval(function(){
+            $(a).css('top', '+=22px');
+            var x = a.clone();
+            $(x).html(randomAscii());
+            $(x).css('color', 'white');
+            matrix.append(x);
+            $(x).fadeOut(2000, function() {
+                $(this).remove();
+            });
+            setTimeout(function() {
+                $(x).css('color', '#20C20E');
+            }, 150)
+            if (Number($(a).css('top').slice(0, -2)) >= window.innerHeight) {
+                clearInterval(rain);
+            }
+            //console.log('length = '+document.getElementsByTagName('a').length+', top = '+$(a).css('top')+', height = '+window.innerHeight);
+            //console.log('boolean = '+(Number($(a).css('top').slice(0, -2)) >= window.innerHeight));
+        }, 150);
+    }
+
+
+
+
+    (function loop() {
+    var randTime = Math.round(Math.random() * (2000 - 999)) + 999;
+    setTimeout(function() {
+    var left = Math.floor(Math.random() * 49)*2 + 'vw';
+    matrixRain(left);
+        console.log(document.getElementsByTagName('a').length);
+            loop();
+    }, randTime);
+    }());
+
+
+
+    /*
     function matrixRain(left) {
         var arr = []
 
@@ -41,21 +97,7 @@ $(window).on('load', function(){
             }
         })
 
-    }
-
-    for (var i=0; i<5; i++) {
-        var left = Math.floor(Math.random() * 49)*2 + 'vw';
-        matrixRain(left);
-    }
-
-
-
-
-
-
-
-
-
+    }*/
 
     /*
     function checkFull() {
